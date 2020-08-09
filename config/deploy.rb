1199ef840de46a6ad8ca4d443a0c978f1d4a897c
carlosElGfe <caguila@miuandes.cl>
2020-08-08 21:48:40 -0400

    comit

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-before 'deploy:assests:precompile','deploy:yarn_install'
+before 'deploy:assets:precompile','deploy:yarn_install'
 namespace :deploy do
     desc ' run it'
     task :yarn_install do
